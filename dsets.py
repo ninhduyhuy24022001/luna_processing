d0d4029fdce4938b5e9cfdba5de91b85a27cb6b8
Toy <ninhduyhuy2001@gmail.com>
2023-05-27 02:26:02 +0700

    percentage_nodule

---
+++ dsets.py
@@ -0,0 +1,143 @@
+import copy
+import csv
+import functools
+import glob
+import os
+
+from collections import namedtuple
+
+import SimpleITK as sitk
+import numpy as np
+
+import torch
+import torch.cuda
+from torch.utils.data import Dataset
+
+from util import XyzTuple, xyz2irc
+
+# config
+CONFIG = {
+    "luna16":"../../luna16/LUNA16/",
+    "segment":"../../luna16/seg-lungs-LUNA16/",
+    "annotations":"../../luna16/annotations.csv",
+    "candidates":"../../luna16/candidates.csv",
+    "slide_v1":"../center-coord-chunk",
+    "slide_v2":"../center-coord-chunk-v2",
+}
+
+# new 
+CandidateInfoTuple = namedtuple(
+    'CandidateInfoTuple',
+    'isNodule_bool, diameter_mm, series_uid, center_xyz',
+)
+
+def getCandidateInfoList():
+    # We construct a set with all series_uids that are present on disk.
+    # This will let us use the data, even if we haven't downloaded all of
+    # the subsets yet.
+    mhd_list = glob.glob(f'{CONFIG["luna16"]}subset*/*.mhd')
+    presentOnDisk_set = {os.path.split(p)[-1][:-4] for p in mhd_list}
+
+    diameter_dict = {}
+    with open(CONFIG["annotations"], "r") as f:
+        for row in list(csv.reader(f))[1:]:
+            series_uid = row[0]
+            annotationCenter_xyz = tuple([float(x) for x in row[1:4]])
+            annotationDiameter_mm = float(row[4])
+
+            diameter_dict.setdefault(series_uid, []).append(
+                (annotationCenter_xyz, annotationDiameter_mm)
+            )
+
+    candidateInfo_list = []
+    with open(CONFIG["candidates"], "r") as f:
+        for row in list(csv.reader(f))[1:]:
+            series_uid = row[0]
+
+            if series_uid not in presentOnDisk_set and requireOnDisk_bool:
+                continue
+
+            isNodule_bool = bool(int(row[4]))
+            candidateCenter_xyz = tuple([float(x) for x in row[1:4]])
+
+            candidateDiameter_mm = 0.0
+            for annotation_tup in diameter_dict.get(series_uid, []):
+                annotationCenter_xyz, annotationDiameter_mm = annotation_tup
+                for i in range(3):
+                    delta_mm = abs(candidateCenter_xyz[i] - annotationCenter_xyz[i])
+                    if delta_mm > annotationDiameter_mm / 4:
+                        break
+                else:
+                    candidateDiameter_mm = annotationDiameter_mm
+                    break
+
+            candidateInfo_list.append(CandidateInfoTuple(
+                isNodule_bool,
+                candidateDiameter_mm,
+                series_uid,
+                candidateCenter_xyz,
+            ))
+
+    candidateInfo_list.sort(reverse=True)
+    return candidateInfo_list
+
+
+class Ct:
+    def __init__(self, series_uid, normalization=True):
+        mhd_path = glob.glob(
+            f'{CONFIG["luna16"]}subset*/{series_uid}.mhd'
+        )[0]
+        # print(mhd_path)
+
+        ct_mhd = sitk.ReadImage(mhd_path)
+        ct_a = np.array(sitk.GetArrayFromImage(ct_mhd), dtype=np.float32)
+
+        # CTs are natively expressed in https://en.wikipedia.org/wiki/Hounsfield_scale
+        # HU are scaled oddly, with 0 g/cc (air, approximately) being -1000 and 1 g/cc (water) being 0.
+        # The lower bound gets rid of negative density stuff used to indicate out-of-FOV
+        # The upper bound nukes any weird hotspots and clamps bone down
+        if normalization:
+            ct_a.clip(-1000, 1000, ct_a)
+
+        self.series_uid = series_uid
+        self.hu_a = ct_a
+
+        self.origin_xyz = XyzTuple(*ct_mhd.GetOrigin())
+        self.vxSize_xyz = XyzTuple(*ct_mhd.GetSpacing())
+        self.direction_a = np.array(ct_mhd.GetDirection()).reshape(3, 3)
+
+    def getRawCandidate(self, center_xyz, width_irc):
+        center_irc = xyz2irc(
+            center_xyz,
+            self.origin_xyz,
+            self.vxSize_xyz,
+            self.direction_a,
+        )
+
+        # test centerpoint
+        # self.hu_a[center_irc[0] - 10: center_irc[0] + 10, center_irc[1] - 10: center_irc[1] + 10, center_irc[2] - 10: center_irc[2] + 10] = 1000
+
+        slice_list = []
+        for axis, center_val in enumerate(center_irc):
+            start_ndx = int(round(center_val - width_irc[axis]/2))
+            end_ndx = int(start_ndx + width_irc[axis])
+
+            assert center_val >= 0 and center_val < self.hu_a.shape[axis], repr([self.series_uid, center_xyz, self.origin_xyz, self.vxSize_xyz, center_irc, axis])
+
+            if start_ndx < 0:
+                # log.warning("Crop outside of CT array: {} {}, center:{} shape:{} width:{}".format(
+                #     self.series_uid, center_xyz, center_irc, self.hu_a.shape, width_irc))
+                start_ndx = 0
+                end_ndx = int(width_irc[axis])
+
+            if end_ndx > self.hu_a.shape[axis]:
+                # log.warning("Crop outside of CT array: {} {}, center:{} shape:{} width:{}".format(
+                #     self.series_uid, center_xyz, center_irc, self.hu_a.shape, width_irc))
+                end_ndx = self.hu_a.shape[axis]
+                start_ndx = int(self.hu_a.shape[axis] - width_irc[axis])
+ 
+            slice_list.append(slice(start_ndx, end_ndx))
+
+        ct_chunk = self.hu_a[tuple(slice_list)]
+
+        return ct_chunk, center_irc
--- util.py
+++ util.py
@@ -1,7 +1,63 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import collections
+from matplotlib.patches import Rectangle
+from PIL import Image
+import glob
+
+config_paths = {
+    "luna16":"../../luna16/LUNA16/",
+    "segment":"../../luna16/seg-lungs-LUNA16/",
+    "annotations":"../../luna16/annotations.csv",
+    "candidates":"../../luna16/candidates.csv",
+    "slide_v1":"../center-coord-chunk",
+    "slide_v2":"../center-coord-chunk-v2",
+}
+
+IrcTuple = collections.namedtuple('IrcTuple', ['index', 'row', 'col'])
+XyzTuple = collections.namedtuple('XyzTuple', ['x', 'y', 'z'])
+
+def irc2xyz(coord_irc, origin_xyz, vxSize_xyz, direction_a):
+    cri_a = np.array(coord_irc)[::-1]
+    origin_a = np.array(origin_xyz)
+    vxSize_a = np.array(vxSize_xyz)
+    coords_xyz = (direction_a @ (cri_a * vxSize_a)) + origin_a
+    # coords_xyz = (direction_a @ (idx * vxSize_a)) + origin_a
+    return XyzTuple(*coords_xyz)
+
+def xyz2irc(coord_xyz, origin_xyz, vxSize_xyz, direction_a):
+    origin_a = np.array(origin_xyz)
+    vxSize_a = np.array(vxSize_xyz)
+    coord_a = np.array(coord_xyz)
+    cri_a = ((coord_a - origin_a) @ np.linalg.inv(direction_a)) / vxSize_a
+    cri_a = np.round(cri_a)
+    return IrcTuple(int(cri_a[2]), int(cri_a[1]), int(cri_a[0]))
+
+
 
 def plot_ct(image, index):
+    """
+    image: np.array of iamge
+    index: list index have mask value
+    """
+    num_fig = int(np.ceil(np.sqrt(len(index))))
+
+   
+    fig, axs = plt.subplots(num_fig, num_fig, figsize=(20,14))
+    slide = 0
+    for x in range(num_fig):
+        for y in range(num_fig):
+            try:
+                axs[x, y].imshow(image[index[slide]])
+                axs[x, y].set_title(index[slide])
+                slide += 1
+            except:
+                pass
+            
+    plt.show()
+    
+
+def plot_ct_scan_bbox(image, index, center_irc, diamiter):
     """
     image: np.array of iamge
     index: list index have mask value
@@ -16,8 +72,24 @@ def plot_ct(image, index):
             try:
                 axs[x, y].imshow(image[index[slide]])
                 axs[x, y].set_title(index[slide])
+                axs[x, y].gca().add_patch(Rectangle((center_irc[2], center_irc[1]), diamiter, diamiter,
+                    edgecolor='red',
+                    facecolor='none',
+                    lw=4))
+
                 slide += 1
             except:
                 pass
             
     plt.show()
+
+def get_uids(subsets = list(range(10))):
+    result = []
+
+    for subset in subsets:
+        files = glob.glob(config_paths["luna16"] + f"subset{subset}/*.mhd")
+
+        for f in files:
+            result.append(f.split("\\")[-1][:-4])
+
+    return result
